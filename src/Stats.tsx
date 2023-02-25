d5857bfac4aebc838a5e4fb7fc3e9c555a5db749
Julian L <julian.lawrance@gmail.com>
2023-02-25 12:32:08 +0000

    Improve appearance of stats boxes

--- src/Stats.tsx
+++ src/Stats.tsx
@@ -8,10 +8,19 @@ export const Stats = (props: StatsProps) => {
     let total = props.cells.map(a => a.value).reduce((a, b) => a + b)
     let voltorbCount = props.cells.filter(a => a.value === 0).length
 
+    let voltorbImage = process.env.PUBLIC_URL + "/voltorb.png"
+
     return (
         <div className="flip-grid-cell">
             <div className="stats">
-                <div>{total}, {voltorbCount}</div>
+                <div className="total-indicator">
+                    {total}
+                </div>
+
+                <div className="voltorb-indicator">
+                    <div>{voltorbCount}</div>
+                    <img src={voltorbImage} alt="voltorb" />
+                </div>
             </div>
         </div>
     )
